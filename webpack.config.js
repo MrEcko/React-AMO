2a9cdf81e4bc73969312edb537bc0aa98970148f
Laurentiu <laurentiu.lucaci@gmail.com>
2019-08-30 14:15:18 +0200

    Added React

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
 var path = require('path');
 
+const jsxRule = {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    loader: "babel-loader",
+    options: { presets: ["@babel/env"] }
+}
+
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
+    mode: 'developments',
+    module: {
+        rules: [jsxRule]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'react-amo.bundle.js'
